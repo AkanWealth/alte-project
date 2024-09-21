7810c63db1c42feb294b39fbad6bf5d32cf69698
Akanwealth Asanga <akan.asanga@gmail.com>
2024-09-21 15:11:00 +0100

    test

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -3,7 +3,7 @@ import { IconButton } from "../../components/Button";
 import Service from "../../components/serviceItem";
 import Testimonial from "../../components/Testimonial";
 import { TestimonialsContent } from "../../contents/testimonials";
-import { servicesContent } from "../../contents/caseStudies";
+import { servicesContent } from "../../contents/Services";
 import Swipers from "../Swipers";
 import { SwiperSlide } from "swiper/react";
 import QuoteForm from "../../components/QuoteForm";
