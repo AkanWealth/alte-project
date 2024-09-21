b1a8e5720d9da97363d657470fb8c145c09acf6f
Akanwealth Asanga <akan.asanga@gmail.com>
2024-09-21 15:56:30 +0100

    compo

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'node:path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx'],
     alias: {
       // Create an alias '@' to reference the 'src' folder
       '@': path.resolve(__dirname, './src'),
