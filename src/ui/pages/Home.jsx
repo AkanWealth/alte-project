8a1a005ea72cf80f35f8904d5413b460a2f48a34
Akanwealth Asanga <akan.asanga@gmail.com>
2024-09-21 15:22:06 +0100

    fixing

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { IconButton } from "../../components/Button";
-import Service from "../../components/service";
+import Service from "../../components/service.jsx";
 import Testimonial from "../../components/Testimonial";
 import { TestimonialsContent } from "../../contents/testimonials";
-import { servicesContent } from "../../contents/Services";
+import { servicesContent } from "../../contents/Services.js";
 import Swipers from "../Swipers";
 import { SwiperSlide } from "swiper/react";
 import QuoteForm from "../../components/QuoteForm";
