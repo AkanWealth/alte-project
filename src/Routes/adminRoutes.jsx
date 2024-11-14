f186317e3f64b28a49b42e21d33d75f600e5dff3
Akanwealth Asanga <akan.asanga@gmail.com>
2024-11-14 20:26:18 +0100

    push

--- src/Routes/adminRoutes.jsx
+++ src/Routes/adminRoutes.jsx
@@ -7,23 +7,23 @@ import UsersLayout from "../features/admin/ui/users/UsersLayout";
 import ListingsLayout from "../features/admin/ui/listings/ListingsLayout";
 
 // Pages
-import AdminLogin from "../features/admin/pages/AdminLogin";
-import AdminSignup from "../features/admin/pages/AdminSignup";
 import Users from "../features/admin/pages/Users";
-import Listings from "../features/admin/pages/Listings";
+import Job from "../features/admin/pages/jobs/Job";
+import Jobs from "../features/admin/pages/jobs/Jobs";
 import Content from "../features/admin/pages/Content";
+import Listings from "../features/admin/pages/Listings";
 import Settings from "../features/admin/pages/Settings";
-import Freelancers from "../features/admin/pages/freelancers/Freelancers";
-import Freelancer from "../features/admin/pages/freelancers/Freelancer";
-import Clients from "../features/admin/pages/clients/Clients";
+import AdminLogin from "../features/admin/pages/AdminLogin";
 import Client from "../features/admin/pages/clients/Client";
+import AdminSignup from "../features/admin/pages/AdminSignup";
+import Clients from "../features/admin/pages/clients/Clients";
+import Project from "../features/admin/pages/projects/Project";
 import InternalUsers from "../features/admin/pages/InternalUsers";
 import Projects from "../features/admin/pages/projects/Projects";
-import Project from "../features/admin/pages/projects/Project";
-import ProjectApplications from "../features/admin/pages/projects/ProjectApplications";
-import Jobs from "../features/admin/pages/jobs/Jobs";
-import Job from "../features/admin/pages/jobs/Job";
+import Freelancer from "../features/admin/pages/freelancers/Freelancer";
+import Freelancers from "../features/admin/pages/freelancers/Freelancers";
 import JobApplications from "../features/admin/pages/jobs/JobApplications";
+import ProjectApplications from "../features/admin/pages/projects/ProjectApplications";
 
 const adminRoutes = {
   path: "admin",
